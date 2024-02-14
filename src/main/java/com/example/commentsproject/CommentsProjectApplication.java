229aadc3c830c5c276876758723ecd2f916e3f0a
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-02-14 13:38:59 +0300

    add gitignore

---
+++ src/main/java/com/example/commentsproject/CommentsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.commentsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commentsproject/CommentsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commentsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
