ab726a3437378989fb92622b0bc3bc711b2f004e
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-03-05 00:52:58 +0300

    feat: add changes

--- src/main/java/by/clevertec/commentsproject/config/OpenApiConfig.java
+++ src/main/java/by/clevertec/commentsproject/config/OpenApiConfig.java
@@ -34,7 +34,7 @@ public class OpenApiConfig {
         contact.url("https://github.com/rusakovich-viktar/news-management-system");
 
         Info info = new Info()
-                .title("Comments system management API")
+                .title("Comments-part API")
                 .version("1.0")
                 .contact(contact);
 
